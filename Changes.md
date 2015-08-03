c62523a92b5e77559b1274ec741f240199cf1dca
smartameer <pradeeppatro16@gmail.com>
2015-08-03 23:33:45 +0530

    Initial Commit

---
+++ Changes.md
@@ -0,0 +1,18 @@
+# Changes
+
+This file is a manually maintained list of changes for each release.
+Feel free to add your changes here when sending pull requests.
+Also send corrections if you spot any mistakes.
+
+## v0.0.1 (2015-07-22)
+
+* Creating Basic jira fetching api for projects
+* @TODO
+* :- parse url to check if it contains http or https
+* :- try catch for argument list
+* :- callback for everything
+* :- eventemiiter before fetching start, fetching inprogress, fetching completed
+* :- ssl certificate access
+* :- restapi authorization auto fetch if username && password given
+* :- authorization on expiry recreate to continue the connection
+* :- waiting time if url blocks for data fetching overload
---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * jira
+ * Copyright(c) 2014-2015
+ * @author: Pradeep Patro <pradeeppatro16@gmail.com>
+ *          Lovely Upadhyay <lovely.upadhyay@gmail.com>
+ * MIT Licensed
+ */
+(function () {
+    'use strict';
+    var Classes = Object.create(null);
+
+    /**
+     * The type constants.
+     * @public
+     */
+    Object.defineProperty(exports, 'Types', {
+        get: loadClass.bind(null, 'Types')
+    });
+
+    /**
+     * Create a new Connection.
+     * @param {object} config
+     * @public
+     */
+    exports.connect = function connect (config) {
+        var JiraConnect = loadClass('jiraConnect');
+        var JiraConfig = loadClass('jiraConfig');
+        return new JiraConnect({config: new JiraConfig(config)});
+    };
+    /**
+     * Load the given class.
+     * @private
+     */
+    function loadClass(className) {
+        var Class = Classes[className];
+
+        if (Class !== undefined) {
+            return Class;
+        }
+
+        // This uses a switch for static require analysis
+        switch (className) {
+            case 'jiraConnect':
+                Class = require('./lib/jiraConnect');
+                break;
+            case 'jiraConfig':
+                Class = require('./lib/jiraConfig');
+                break;
+            default:
+                throw new Error('Cannot find class \'' + className + '\'');
+        }
+
+        // Store to prevent invoking require()
+        Classes[className] = Class;
+
+        return Class;
+    }
+}());
